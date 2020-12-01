f35d02062b1448af023cb9e6d732b381b3d8bf63
hurui <571469164@qq.com>
2020-12-01 14:09:56 +0800

    login2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        登录组件
+    <div class="login_container">
+        <div class="login_box"></div>
     </div>
 </template>
 <script>
@@ -9,5 +9,19 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+.login_container {
+    position: relative;
+    background-color: #2B4B6B;
+    height: 100%;
+}
+.login_box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 450px;
+    height: 300px;
+    background-color: #FFFFFF;
+    border-radius: 3px;
+}
 </style>
