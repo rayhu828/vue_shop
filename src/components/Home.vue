a0a3ba9d8e1dc0d7fc86adae29e4c442ea7a5394
hurui <571469164@qq.com>
2020-12-06 13:38:47 +0800

    home2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,10 @@
   <el-container class="home-container">
     <!-- 头部区域 -->
     <el-header>
-      Header
+      <div>
+        <img src="../assets/heima.png"/>
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
@@ -30,6 +33,19 @@ export default {
 }
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #FFFFFF;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    > img {
+      margin-right: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
