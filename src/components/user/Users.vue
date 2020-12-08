0580e834ed783c2772807a256206233873c30004
hurui <571469164@qq.com>
2020-12-09 00:10:09 +0800

    user6

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -85,9 +85,27 @@
       >
       </el-pagination>
       <!-- 添加用户的对话框 -->
-      <el-dialog title="提示" :visible.sync="addDialogVisible" width="50%">
+      <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClose">
         <!-- 内容主体区域 -->
-        <span>这是一段信息</span>
+        <el-form
+          :model="addForm"
+          :rules="addFormRules"
+          ref="addFormRef"
+          label-width="70px"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="addForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="addForm.password"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="addForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机" prop="mobile">
+            <el-input v-model="addForm.mobile"></el-input>
+          </el-form-item>
+        </el-form>
         <!-- 底部区域 -->
         <span slot="footer" class="dialog-footer">
           <el-button @click="addDialogVisible = false">取 消</el-button>
@@ -102,6 +120,22 @@
 <script>
 export default {
   data: function () {
+    var checkEmail = (rule, value, callback) => {
+      // 验证邮箱的正则表达式
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入合法的邮箱'))
+    }
+    var checkMobile = (rule, value, callback) => {
+      //验证手机号的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入合法的手机号'))
+    }
     return {
       queryInfo: {
         query: '',
@@ -112,6 +146,62 @@ export default {
       pagenum: 1,
       total: 0,
       addDialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: '',
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur',
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '用户名长度在3到10个字符之间',
+            trigger: 'blur',
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur',
+          },
+          {
+            min: 6,
+            max: 15,
+            message: '密码长度在6到15个字符之间',
+            trigger: 'blur',
+          },
+        ],
+        email: [
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur',
+          },
+          {
+            validator: checkEmail,
+            trigger: 'blur',
+          },
+        ],
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机',
+            trigger: 'blur',
+          },
+          {
+            validator: checkMobile,
+            trigger: 'blur',
+          },
+        ],
+      },
     }
   },
   methods: {
@@ -147,6 +237,9 @@ export default {
       }
       this.$message.success(res.meta.msg)
     },
+    addDialogClose: function() {
+      this.$refs.addFormRef.resetFields()
+    }
   },
   created: function () {
     this.getUserList()
