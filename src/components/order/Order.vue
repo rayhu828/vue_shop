f2055d9b8f319f99cb4af176284d0a0aab09904e
hurui <571469164@qq.com>
2020-12-19 22:42:44 +0800

    order1

---
+++ src/components/order/Order.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" :clearable="true">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      total: 0,
+      orderList: []
+    }
+  },
+  methods: {
+    getOrderList: async function() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.total = res.data.total
+      this.orderList = res.data.goods
+    }
+  },
+  created: function() {
+    this.getOrderList()
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
